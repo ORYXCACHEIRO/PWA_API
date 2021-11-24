7174a866a1762de5345bc1143e732eb04658069e
Silanthris <47530239+Silanthris@users.noreply.github.com>
2021-11-24 17:12:38 +0000

    search
    aaaaaaaaaaaaaaa

--- api/router.js
+++ api/router.js
@@ -5,6 +5,7 @@ let langsAPI = require('./routes/langs/langsAdmin');
 let usersAPI = require('./routes/user');
 let authAPI = require('./routes/auth');
 let profAPI = require('./routes/profile');
+let searchAPI = require('./routes/search');
 
 function initialize(){
     let api = express();
@@ -15,6 +16,7 @@ function initialize(){
     api.use('/users', usersAPI());
     api.use('/auth', authAPI());
     api.use('/profile', profAPI());
+    api.use('/search', searchAPI());
 
     return api;
 }
